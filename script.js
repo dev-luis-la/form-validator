3843a4daa57f8f453e2d32f774f61f2298fcbaf4
dev-luis-la <92961052+dev-luis-la@users.noreply.github.com>
2022-03-10 15:26:53 -0500

    creating validate functions

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const form = document.getElementById('form');
-const name = document.getElementById('name');
+const password1 = document.getElementById('password1');
+const password2 = document.getElementById('password2');
 
+let isValid = false;
 
-
-//get info from form 
-
-function submitForm(){
+function validateForm(){
+    //usiing constraint API
+    isValid = form.checkValidity();
     
 }
+//get info from form 
+function formData(e){
+    e.preventDefault();
+    //validate form
+    validateForm();
+}
+
+
+//event listner
+
+form.addEventListener('submit', formData);
