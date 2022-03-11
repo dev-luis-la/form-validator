08efdeb3d9646bd1dd5af7ed17a29c01db12fd5f
dev-luis-la <92961052+dev-luis-la@users.noreply.github.com>
2022-03-11 12:18:19 -0500

    validation done

--- script.js
+++ script.js
@@ -12,32 +12,48 @@ function validateForm(){
     if (!isValid) {
         message.textContent = 'Please check All Fields';
         message.style.color = 'red';
+        return;
     }
     //check if passwords match
     if (password1.value === password2.value) {
         passwordsMatch = true;
         password1.style.borderColor = 'green';
         password2.style.borderColor = 'green';
-        message.textContent = 'Youre all set'
-        message.style.color = 'green'
+        message.textContent = 'All set';
+        message.style.color = 'green';
     } else {
         passwordsMatch = false;
         password1.style.borderColor = 'red';
         password2.style.borderColor = 'red';
-        message.textContent = 'Passwords dont match'
-        message.style.color = 'red'
+        message.textContent = 'Passwords dont match';
+        message.style.color = 'red';
+        return;
     }
+    //form valid and password match
     if (isValid && passwordsMatch){
         console.log('sucess');
-    }
-    
+    }   
+}
+function storeFormData(){
+    const user = {
+        name: form.name.value,
+        lastName: form.lastName.value,
+        phone:  form.phone.value,
+        email: form.email.value,
+        password: form.password.value
+    };
+    //do something with user data
 }
 //get info from form 
 function formData(e){
     e.preventDefault();
     //validate form
     validateForm();
+    if (isValid && passwordsMatch) {
+        storeFormData();
+    }
 }
+   
 
 
 //event listner
